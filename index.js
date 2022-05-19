594029aa9bbdea469dac7d7805ee7772e40d7661
zaak <zaak.kerstetter@gmail.com>
2022-05-19 14:54:16 -0600

    commit tree 95a39dbb62e19baf154115a65efb3e665e6d01f9

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("text").textContent = ("This is really cool!")
+}
+
+)
+
+// function updateDom(){
+//     document.getElementById("text").textContent = ("This is really cool!")
+// }
